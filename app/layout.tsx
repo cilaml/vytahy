4924d7a28902ba1c800fcd251775469cfd68a70b
cilaml <ptrollstva@gmail.com>
2026-07-28 20:40:20 +0200

    Show every active technician on dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import GlobalSidebar from "@/components/GlobalSidebar";
 import GlobalChrome from "@/components/GlobalChrome";
 import TechnicianAvailabilityManagerSafe from "@/components/TechnicianAvailabilityManagerSafe";
+import TechnicianDashboardExpander from "@/components/TechnicianDashboardExpander";
 import "./globals.css";
 import "./modern-overrides.css";
 import "./chrome-fixes.css";
@@ -35,6 +36,7 @@ export default function RootLayout({
       <body className="min-h-full">
         <GlobalSidebar />
         <GlobalChrome />
+        <TechnicianDashboardExpander />
         <TechnicianAvailabilityManagerSafe />
         <div className="app-content-root">{children}</div>
       </body>
